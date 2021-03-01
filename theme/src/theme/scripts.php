5cb037b925553e0a5a8721db2976e549266e3fa9
RobVermeer <rob@vermeertech.nl>
2021-03-01 20:38:20 +0100

    Make sure that the ajaxurl is accessible to the client
    This url is needed to send requests to, coming from JS.

--- theme/src/theme/scripts.php
+++ theme/src/theme/scripts.php
@@ -2,6 +2,7 @@
 
 function theme_scrips() {
   wp_enqueue_script('theme-script', get_bloginfo('stylesheet_directory') . '/app.js', false, null, true);
+  wp_localize_script('theme-script', 'ajax_object', array('ajax_url' => admin_url('admin-ajax.php')));
 }
 
 add_action('wp_enqueue_scripts', 'theme_scrips');
